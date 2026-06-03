1b509d8b33f03860a73fbb45ddc54472314fa467
Breeti Bandyopadhyay <bandybreeti@gmail.com>
2026-06-03 19:23:55 +0530

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s2)<len(s1):
+            return False
+
+        freq=[0]*26
+        curfreq=[0]*26
+        for i in range(len(s1)):
+            element=ord(s1[i])%26
+            freq[element]+=1
+            element=ord(s2[i])%26
+            curfreq[element]+=1
+        
+        if curfreq==freq:
+            return True
+        
+        last=0
+        for i in range(len(s1),len(s2)):
+            current=ord(s2[i])%26
+            prev=ord(s2[last])%26
+            curfreq[current]+=1
+            curfreq[prev]-=1
+            last+=1
+            if curfreq==freq:
+                return True
+
+        return False
+            

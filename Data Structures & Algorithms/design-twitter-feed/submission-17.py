679bb75fd5569adc1cc3051c20f9a39f297d3ac3
Breeti Bandyopadhyay <bandybreeti@gmail.com>
2026-06-07 22:35:32 +0530

    Add: design-twitter-feed - submission-17

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-17.py
@@ -0,0 +1,28 @@
+class Twitter:
+
+    def __init__(self):
+        self.following=[set() for _ in range(101)]
+        self.all_tweets=[]
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.all_tweets.append([tweetId,userId])
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        relavant=self.following[userId]
+        #relavant.add(userId)
+        res=[]
+        count=0
+        for i in self.all_tweets[::-1]:
+            if i[1] in relavant or i[1]==userId:
+                res.append(i[0])
+                count+=1
+                if count==10:
+                    break
+        return (res)
+            
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.following[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:  
+        self.following[followerId].discard(followeeId)

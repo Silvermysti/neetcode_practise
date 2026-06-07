5ff7fe30f42dbdcb9251178303cd017150196c42
Breeti Bandyopadhyay <bandybreeti@gmail.com>
2026-06-07 21:38:28 +0530

    Add: design-twitter-feed - submission-3

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-3.py
@@ -0,0 +1,33 @@
+class Twitter:
+
+    def __init__(self):
+        self.following=[set() for _ in range(101)]
+        self.tweets=[[] for _ in range(101)]
+        
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweets[userId].append(tweetId)     
+        print(userId, " Tweeted Id: ", tweetId)
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        # ✅ Convert set to list
+        res = []
+        for i in self.following[userId]:
+            res.extend(self.tweets[i])
+        res.extend(self.tweets[userId])
+        
+        print("User ", userId, "'s feed: ", res)
+        return res
+            
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId==followeeId:
+            return
+        self.following[followerId].add(followeeId)
+        print(followerId, " Started Following ", followeeId, " now following: ", self.following[followerId])
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:  
+        if followerId==followeeId:
+            return   
+        self.following[followerId].remove(followeeId)
+        print(followerId, " Unfollowed ", followeeId, " now following: ", self.following[followerId])

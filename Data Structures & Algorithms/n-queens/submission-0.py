72b0a0ba16f06bd715876d7531437ca9c5349cc7
Breeti Bandyopadhyay <bandybreeti@gmail.com>
2026-06-08 23:38:44 +0530

    Add: n-queens - submission-0

---
+++ Data Structures & Algorithms/n-queens/submission-0.py
@@ -0,0 +1,41 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        res=[]
+        positions=[['.' for _ in range(n)] for _ in range(n)]
+        #print(positions)
+        available=[set(range(n)) for _ in range(n)]
+
+        def backtrack(available,row):
+            #print(row, positions)
+            if row==n:
+                #print(positions)
+                strings=["".join(s.copy()) for s in positions]
+                #print(strings)
+                res.append(strings)
+                #print(res)
+                return
+            elif len(available[n-1])==0:
+                return
+
+            for col in available[row]:
+                print(row,col)
+                positions[row][col]='Q'
+                temp = [s.copy() for s in available]
+                for j in range(row+1,n):
+                    temp[j].discard(col)
+                i=1
+                while row+i<n:
+                    if col+i<n:
+                        temp[row+i].discard(col+i)
+                    if col-i>=0:
+                        temp[row+i].discard(col-i)
+                    i+=1
+                backtrack(temp,row+1)
+                positions[row][col]='.'
+
+        
+
+
+        backtrack(available.copy(),0)
+        return res
+

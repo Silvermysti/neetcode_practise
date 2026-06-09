72259b30ddb7c66a30e3373a6a12ba7b439a88d2
Breeti Bandyopadhyay <bandybreeti@gmail.com>
2026-06-09 16:20:31 +0530

    Add: implement-prefix-tree - submission-4

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-4.py
@@ -0,0 +1,39 @@
+class PrefixTree:
+
+    def __init__(self):
+        self.root={}
+
+    def insert(self, word: str) -> None:
+        node=self.root
+
+        for i in word:
+            node=node.setdefault(i,{})
+        node["is_end"]=True
+        #print(self.root)
+        
+
+    def search(self, word: str) -> bool:
+        node=self.root
+        length=len(word)
+        i=0
+        while i<length and word[i] in node:
+            node=node[word[i]]
+            i+=1
+
+        if i==length and "is_end" in node:
+            return True
+        else:return False
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        node=self.root
+        length=len(prefix)
+        i=0
+        while i<length and prefix[i] in node:
+            node=node[prefix[i]]
+            i+=1
+
+        if i<length:
+            return False
+        else:return True
+        

317ea3d1f8f11825848a4c7f4d6e2288b9f5560c
Breeti Bandyopadhyay <bandybreeti@gmail.com>
2026-06-16 23:56:18 +0530

    Add: max-area-of-island - submission-1

---
+++ Data Structures & Algorithms/max-area-of-island/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        
+        maxarea=0
+        netcol=len(grid[0])
+        netrow=len(grid)
+        visited=[[0]*netcol for _ in range*networ]
+        print(visited)
+        return 2

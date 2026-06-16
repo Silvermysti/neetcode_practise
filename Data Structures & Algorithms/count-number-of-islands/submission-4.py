b9d2d1e71ff08fa8e971b09a81cc8ee2431f37a6
Breeti Bandyopadhyay <bandybreeti@gmail.com>
2026-06-16 23:52:31 +0530

    Add: count-number-of-islands - submission-4

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-4.py
@@ -0,0 +1,35 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        
+        visited=[[0]*len(grid[0]) for _ in range(len(grid))]
+        netcols=len(grid[0])
+        netrows=len(grid)
+
+
+        def traverse(row,col):
+
+            visited[row][col]=1
+            for i in visited:
+                print(i)
+
+            if row>0 and visited[row-1][col]!=1 and grid[row-1][col]!="0":
+                traverse(row-1,col)
+            if row+1<netrows and visited[row+1][col]!=1 and grid[row+1][col]!="0":
+                traverse(row+1,col)
+            if col>0 and visited[row][col-1]!=1 and grid[row][col-1]!="0":
+                traverse(row,col-1)
+            if col+1<netcols and visited[row][col+1]!=1 and grid[row][col+1]!="0":
+                traverse(row,col+1)
+            return
+
+
+        res=0
+        for row in range(netrows):
+            for col in range(netcols):
+                if grid[row][col]=="1" and visited[row][col]==0:
+                    res+=1
+                    traverse(row, col)
+                    
+        
+
+        return res

ee416a6688eabf6b7858d61c697561063b44f8ae
Breeti Bandyopadhyay <bandybreeti@gmail.com>
2026-06-28 02:55:38 +0530

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        
+        self.mindis=1000
+        length=len(nums)-1
+
+
+        def traverse(currind,jumps):
+
+            if currind>=length:
+                self.mindis=min(self.mindis,jumps)
+                #print(self.mindis,currind)
+                return
+            if nums[currind]==0:
+                return
+
+            for i in range(1, nums[currind]+1):
+                #print("traverse(",currind+i," , ",jumps+1," )")
+                    traverse(currind+i,jumps+1)
+        
+
+        traverse(0,0)
+
+        return self.mindis
+            
+            

220abb10dfc49fa51e695253261e02e0bbabc52f
Breeti Bandyopadhyay <bandybreeti@gmail.com>
2026-06-30 05:44:52 +0530

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        graph=defaultdict(set)
+        for i,j in prerequisites:
+            if (j not in graph) or (i not in graph[j]):
+                graph[i].add(j)
+            else:
+                return False
+        #print(graph)
+
+        visited=set()
+        traversed=set()
+
+        def traverse(node):
+            traversed.add(node)
+            if node in graph:
+                for i in graph[node]:
+                    if i in visited:
+                        return True
+                    visited.add(i)
+                    if traverse(i):
+                        return True
+                    visited.remove(i)
+            return False
+
+        for node in graph.keys():
+            if node not in traversed:
+                visited.add(node)
+                if traverse(node):
+                    return False
+                visited.remove(node)
+        return True

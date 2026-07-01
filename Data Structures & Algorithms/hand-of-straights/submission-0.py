33d294cbcf1d25d46304570bce5acc242ee0f9fb
Breeti Bandyopadhyay <bandybreeti@gmail.com>
2026-07-02 01:11:36 +0530

    Add: hand-of-straights - submission-0

---
+++ Data Structures & Algorithms/hand-of-straights/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+
+        if len(hand)%groupSize!=0:
+            return False
+        
+        hand.sort()
+        groups=deque()
+        ngroups=0
+
+        ind=0
+        base=0
+
+        for i in hand:
+            while ind<len(groups) and groups[ind][-1]==i:
+                ind+=1
+
+            if ind==len(groups):
+                groups.append([i])
+            elif groups[ind][-1]==i-1:
+                groups[ind].append(i)
+            else:
+                return False
+
+            if len(groups[base])==4:
+                base+=1
+            ind=base
+            
+            #print(groups)
+        
+        return True

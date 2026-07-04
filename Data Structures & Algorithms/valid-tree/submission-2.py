77fa06aeba0a220ad17589b48f87cb7127f65e83
Breeti Bandyopadhyay <bandybreeti@gmail.com>
2026-07-05 04:40:44 +0530

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        
+        nodes={c:set() for c in range(n)}
+        for i in edges:
+            nodes[i[0]].add(i[1])
+            nodes[i[1]].add(i[0])
+        print(nodes)
+        visited=set()
+        cycle=set()
+
+        def traverse(node,prev):
+            if node in visited:
+                return False
+
+            visited.add(node)
+
+            for i in nodes[node]:
+                if i!=prev:
+                    if traverse(i,node)==False:
+                        print(node,i,visited)
+                        return False
+            return True
+            
+        if traverse(0,None)==False or len(visited)!=n:
+            return False
+
+        return True

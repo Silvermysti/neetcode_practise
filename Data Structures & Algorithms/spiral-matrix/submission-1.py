15c4fbff24fadad386c796fa2a607a6a7310cb14
Breeti Bandyopadhyay <bandybreeti@gmail.com>
2026-07-05 16:41:25 +0530

    Add: spiral-matrix - submission-1

---
+++ Data Structures & Algorithms/spiral-matrix/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        
+        netrow=len(matrix)
+        netcol=len(matrix[0])
+        visited=set()
+        res=[]
+
+        directions=[[0,1],[1,0],[0,-1],[-1,0]]
+        def dfs(row,col,curr):
+            res.append(matrix[row][col])
+            visited.add(matrix[row][col])
+            #print(res)
+            for i in range(4):
+                direction=directions[(curr+i)%4]
+                if -1<row+direction[0]<netrow and -1<col+direction[1]<netcol and matrix[row+direction[0]][col+direction[1]] not in visited:
+                    dfs(row+direction[0],col+direction[1],curr+i)
+                    return
+            return
+
+        dfs(0,0,0)    
+        return res
+
